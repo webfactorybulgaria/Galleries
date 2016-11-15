755972e29f29a3bee02d7d131912858e4cffe013
Nasko <nasko@webfactory.bg>
2016-11-15 12:57:18 +0200

    style

--- src/resources/views/public/_galleries.blade.php
+++ src/resources/views/public/_galleries.blade.php
@@ -1,9 +1,11 @@
 @foreach ($model->galleries as $gallery)
-<div class="gallery">
-    @if($gallery->title)
-    <h3 class="gallery-title">{{ $gallery->title }}</h3>
+    @if($gallery->status)
+    <section class="gallery{{ (!empty($custom_class)) ? ' '.$custom_class : '' }}">
+        @if($gallery->title)
+        <h3 class="gallery-title">{{ $gallery->title }}</h3>
+        @endif
+        {!! $gallery->present()->body !!}
+        @include('galleries::public._thumbs', ['model' => $gallery])
+    </section>
     @endif
-    {!! $gallery->present()->body !!}
-    @include('galleries::public._thumbs', ['model' => $gallery])
-</div>
 @endforeach
--- src/resources/views/public/_thumbs.blade.php
+++ src/resources/views/public/_thumbs.blade.php
@@ -3,8 +3,12 @@
     @foreach ($model->files as $file)
         <li class="files-list-item">
             @if ($file->type == 'i')
-            <a class="files-list-image fancybox" href="{!! $file->present()->thumbSrc(1200, 1200, array('resize'), 'file') !!}" data-fancybox-group="{{ $model->slug }}">
-                <img class="files-list-image-thumb" src="{!! $file->present()->thumbSrc(370, 370, array(), 'file') !!}" alt="{{ $file->alt_attribute }}">
+            <a class="files-list-image fancybox" href="{!! $file->present()->thumbSrc(1200, 1200, array('resize'), 'file') !!}" data-fancybox-group="{{ $model->slug }}" title="{{ $file->alt_attribute }}" data-descr="{{ $file->description }}">
+                <img class="files-list-image-thumb" src="{!! $file->present()->thumbSrc(650, 350, array(), 'file') !!}" alt="{{ $file->alt_attribute }}">
+                <div class="files-list-descr">
+                    <h4>{{ $file->present()->alt_attribute }}</h4>
+                    <p>{{ $file->present()->description }}</p>
+                </div>
             </a>
             @else
             <a class="files-list-document" href="{{ asset($file->path.'/'.$file->file) }}" target="_blank">
